3d88338e5ce3196c6f4ddc40c48d9713f6e9757a
Leonardo <lmagalhaes@exactsales.com.br>
2021-01-21 23:48:45 -0300

    Adicionado testes de serviço

---
+++ test/distribuicao-lucros-api-tests/Controllers/ProfitSharingControllerTest.cs
@@ -0,0 +1,58 @@
+﻿using distribuicao_lucros.Controllers;
+
+using distribuicao_lucros_application.Features.ProfitSharing;
+
+using distribuicao_lucros_domain.Features.ProfitSharing;
+
+using FluentAssertions;
+
+using Microsoft.AspNetCore.Mvc;
+
+using Moq;
+
+using NUnit.Framework;
+
+using System.Threading.Tasks;
+
+namespace distribuicao_lucros_api_tests.Controllers
+{
+    public class ProfitSharingControllerTest
+    {
+        private ProfitSharingController profitSharingController;
+        private Mock<IProfitSharingService> profitSharingServiceMock;
+
+        [SetUp]
+        public void SetUp()
+        {
+            profitSharingServiceMock = new Mock<IProfitSharingService>();
+
+            profitSharingController = new ProfitSharingController(profitSharingServiceMock.Object);
+        }
+
+        [Test]
+        public async Task ProfitSharing_Should_Call_Service_And_Return_Code_200()
+        {
+            double availableValue = 200000;
+
+            IActionResult result = await profitSharingController.Get(availableValue);
+
+            result.Should().BeOfType<OkObjectResult>();
+            profitSharingServiceMock.Verify(p => p.GetProfitSharingResult(availableValue), Times.Once);
+            profitSharingServiceMock.VerifyNoOtherCalls();
+        }
+
+        [Test]
+        public async Task ProfitSharing_Should_Return_BadRequest_When_Throw_InsufficientValueProfitSharingException()
+        {
+            double availableValue = 200000;
+
+            profitSharingServiceMock.Setup(p => p.GetProfitSharingResult(availableValue)).Throws(new InsufficientValueProfitSharingException(string.Empty));
+
+            IActionResult result = await profitSharingController.Get(availableValue);
+
+            result.Should().BeOfType<BadRequestObjectResult>();
+            profitSharingServiceMock.Verify(p => p.GetProfitSharingResult(availableValue), Times.Once);
+            profitSharingServiceMock.VerifyNoOtherCalls();
+        }
+    }
+}
---
+++ test/distribuicao-lucros-application-tests/Features/Employees/EmployeeServiceTest.cs
@@ -0,0 +1,76 @@
+﻿using AutoMapper;
+
+using distribuicao_lucros_application.Features.Employees;
+using distribuicao_lucros_application.Features.Employees.Dto;
+
+using distribuicao_lucros_domain.Features.Employees;
+
+using FluentAssertions;
+
+using Moq;
+
+using NUnit.Framework;
+
+using System.Collections.Generic;
+using System.Threading.Tasks;
+
+namespace distribuicao_lucros_application_tests.Features.Employees
+{
+    public class EmployeeServiceTest
+    {
+        private EmployeeService employeeService;
+        private Mock<IEmployeeRepository> employeeRepositoryMock;
+        private Mock<IMapper> mapperMock;
+
+        [SetUp]
+        public void SetUp()
+        {
+            employeeRepositoryMock = new Mock<IEmployeeRepository>();
+            mapperMock = new Mock<IMapper>();
+
+            employeeService = new EmployeeService(employeeRepositoryMock.Object, mapperMock.Object);
+        }
+
+        [Test]
+        public async Task Add_Employee_Collection_Should_Call_Repository()
+        {
+            var employees = new EmployeeDTO[2];
+
+            var employeesMapped = new Employee[1];
+
+            mapperMock.Setup(m => m.Map<IEnumerable<Employee>>(employees)).Returns(employeesMapped);
+
+            await employeeService.Add(employees);
+
+            employeeRepositoryMock.Verify(e => e.Add(employeesMapped), Times.Once);
+            employeeRepositoryMock.VerifyNoOtherCalls();
+            mapperMock.Verify(m => m.Map<IEnumerable<Employee>>(employees), Times.Once);
+            mapperMock.VerifyNoOtherCalls();
+        }
+
+        [Test]
+        public async Task Delete_Employee_Should_Call_Repository()
+        {
+            await employeeService.Delete();
+
+            employeeRepositoryMock.Verify(e => e.Delete(), Times.Once);
+            employeeRepositoryMock.VerifyNoOtherCalls();
+            mapperMock.VerifyNoOtherCalls();
+        }
+
+        [Test]
+        public async Task GetAll_Employee_Should_Call_Repository_And_Return_Collection()
+        {
+            var employees = new Employee[2];
+
+            employeeRepositoryMock.Setup(e => e.GetAll()).Returns(Task.FromResult<IEnumerable<Employee>>(employees));
+
+            var employeesResult = await employeeService.GetAll();
+
+            employeesResult.Should().BeEquivalentTo(employees);
+            employeeRepositoryMock.Verify(e => e.GetAll(), Times.Once);
+            employeeRepositoryMock.VerifyNoOtherCalls();
+            mapperMock.VerifyNoOtherCalls();
+        }
+    }
+}
